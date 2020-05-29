39665d4bfa93783e1e5dcb02ac1ed15032a2b143
Pranav Sharma <pranav.sharma.ama@gmail.com>
2020-05-29 00:04:59 -0400

    done with chap 3

--- snippetbox/cmd/web/handlers.go
+++ snippetbox/cmd/web/handlers.go
@@ -3,17 +3,16 @@ package main
 import (
     "fmt"
     "html/template"
-    "log"
     "net/http"
     "strconv"
 )
 
 // handler for home
-func home(w http.ResponseWriter, r *http.Request) {
+func (app *application) home(w http.ResponseWriter, r *http.Request) {
 
     // path check
     if r.URL.Path != "/" {
-        http.NotFound(w, r)
+        app.notFound(w)
         return
     }
 
@@ -26,25 +25,23 @@ func home(w http.ResponseWriter, r *http.Request) {
     // try to parse template, throw error if can't
     ts, err := template.ParseFiles(files...)
     if err != nil {
-        log.Println(err.Error())
-        http.Error(w, "Internal Server Error", 500)
+        app.serverError(w, err)
         return
     }
 
     // use execute to write template to response
     err = ts.Execute(w, nil)
     if err != nil {
-        log.Println(err.Error())
-        http.Error(w, "Internal Server Error", 500)
+        app.serverError(w, err)
     }
 
 }
 
 // handler for show snippet
-func showSnippet(w http.ResponseWriter, r *http.Request) {
+func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
     id, err := strconv.Atoi(r.URL.Query().Get("id"))
     if err != nil || id < 1 {
-        http.NotFound(w, r)
+        app.notFound(w)
         return
     }
 
@@ -52,11 +49,10 @@ func showSnippet(w http.ResponseWriter, r *http.Request) {
 }
 
 // handler for create snippet
-func createSnippet(w http.ResponseWriter, r *http.Request) {
+func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
     if r.Method != "POST" {
         w.Header().Set("Allow", "POST")
-        w.WriteHeader(405)
-        w.Write([]byte("Method not allowed"))
+        app.clientError(w, http.StatusMethodNotAllowed)
         return
     }
 
--- snippetbox/cmd/web/main.go
+++ snippetbox/cmd/web/main.go
@@ -1,26 +1,46 @@
 package main
 
 import (
+    "flag"
 	"log"
     "net/http"
+    "os"
 )
+
+type application struct {
+    errorLog *log.Logger
+    infoLog  *log.Logger
+}
 
 
 func main() {
 
+    addr := flag.String("addr", ":4000", "HTTP network address")
+
+    flag.Parse()
+
+    infoLog := log.New(os.Stdout, "[INFO]\t", log.Ldate|log.Ltime)
+
+    errorLog := log.New(os.Stderr, "[ERR]\t",
+        log.Ldate|log.Ltime|log.Lshortfile)
+
+    app := &application{
+        errorLog: errorLog,
+        infoLog:  infoLog,
+    }
+
 	// create new router
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet", showSnippet)
-	mux.HandleFunc("/snippet/create", createSnippet)
+    mux := app.routes()
 
-    fileServer := http.FileServer(http.Dir("./ui/static/"))
-
-    mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+    srv := &http.Server{
+        Addr: *addr,
+        ErrorLog: errorLog,
+        Handler: mux,
+    }
 
 	// start logging and start up webserver
 	// listen and serve should never return unless hit error
-	log.Println("Starting server on :4000")
-	err := http.ListenAndServe(":4000", mux)
-	log.Fatal(err)
+	infoLog.Printf("Starting server on %s", srv.Addr)
+	err := srv.ListenAndServe()
+	errorLog.Fatal(err)
 }
