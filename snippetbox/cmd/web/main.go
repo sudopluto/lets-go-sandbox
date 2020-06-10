ab17332679392937b46a80aa2b3732d2077dfd02
Pranav Sharma <pranav.sharma.ama@gmail.com>
2020-06-10 00:10:14 -0400

    done with 4.4, model abs created

--- snippetbox/cmd/web/main.go
+++ snippetbox/cmd/web/main.go
@@ -7,12 +7,15 @@ import (
     "net/http"
     "os"
 
+    "github.com/sudopluto/lets-go-sandbox/pkg/models/mysql"
+
     _ "github.com/go-sql-driver/mysql"
 )
 
 type application struct {
     errorLog *log.Logger
     infoLog  *log.Logger
+    snippets *mysql.SnippetModel
 }
 
 func openDB(dsn string) (*sql.DB, error) {
@@ -50,6 +53,7 @@ func main() {
     app := &application{
         errorLog: errorLog,
         infoLog:  infoLog,
+        snippets: &mysql.SnippetModel{DB: db},
     }
 
 	// create new router
